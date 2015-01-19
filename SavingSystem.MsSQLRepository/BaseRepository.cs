a58146645399c98643c33f0915ee18c222dad801
Ivan Ponomarenko <ivanponomarenko01@gmail.com>
2015-01-19 10:01:53 +0200

    moved methods from QueryManager.cs to BaseRepository.cs

--- SavingSystem.MsSQLRepository/BaseRepository.cs
+++ SavingSystem.MsSQLRepository/BaseRepository.cs
@@ -13,23 +13,29 @@ namespace SavingSystem.MsSQLRepository
 {
     public abstract class BaseRepository<T> : IRepository<T> where T : class, new()
     {
-        private string cnStr;
-        protected QueryManager QueryManager = new QueryManager();
+        private readonly string _cnStr;
+        protected QueryManager QueryManager;
 
         protected BaseRepository(string cnString = "SavSysEntities")
         {
-            cnStr = ConfigurationManager.ConnectionStrings[cnString].ConnectionString;
+            _cnStr = ConfigurationManager.ConnectionStrings[cnString].ConnectionString;
+            QueryManager = new QueryManager();
         }
+
 
         public IEnumerable<T> GetAll()
         {
             IList<T> items;
-            T type = default(T);
-            IList<PropertyInfo> properties = null;
-            using (SqlConnection connection = new SqlConnection(cnStr))
+            var type = new T();
+            IList<PropertyInfo> properties = type.GetType().GetProperties();
+            var tableName = (TableNameAttribute)type.GetType().GetCustomAttribute(typeof(TableNameAttribute));
+            if (tableName == null) { throw new NullReferenceException("Table name attribute of type cannot be empty or null."); }
+            List<string> columnsStrings = GetColumnsStringsOnAttributesProperties(properties);
+
+            using (var connection = new SqlConnection(_cnStr))
             {
-                var query = QueryManager.GenerateSelectAllQuery(type, ref properties);
-                using (SqlCommand command = new SqlCommand(query, connection))
+                var query = QueryManager.GenerateSelectAllQuery(tableName.Name, columnsStrings);
+                using (var command = new SqlCommand(query, connection))
                 {
                     connection.Open();
                     SqlDataReader reader = command.ExecuteReader();
@@ -40,16 +46,21 @@ namespace SavingSystem.MsSQLRepository
             }
             return items;
         }
+
 
         public IEnumerable<T> GetAll(int profileId)
         {
             IList<T> items;
-            T type = new T();
-            IList<PropertyInfo> properties = null;
-            using (SqlConnection connection = new SqlConnection(cnStr))
+            var type = new T();
+            IList<PropertyInfo> properties = type.GetType().GetProperties();
+            var tableName = (TableNameAttribute)type.GetType().GetCustomAttribute(typeof(TableNameAttribute));
+            if (tableName == null) { throw new NullReferenceException("Table name attribute of type cannot be empty or null."); }
+            List<string> columnsStrings = GetColumnsStringsOnAttributesProperties(properties);
+
+            using (var connection = new SqlConnection(_cnStr))
             {
-                var query = QueryManager.GenerateSelectAllQuery(type, ref properties, profileId);
-                using (SqlCommand command = new SqlCommand(query, connection))
+                var query = QueryManager.GenerateSelectAllQuery(tableName.Name, columnsStrings, profileId);
+                using (var command = new SqlCommand(query, connection))
                 {
                     connection.Open();
                     SqlDataReader reader = command.ExecuteReader();
@@ -60,16 +71,21 @@ namespace SavingSystem.MsSQLRepository
             }
             return items;
         }
+        
 
         public T Get(int profileId, int fieldId)
         {
             IList<T> items;
-            T type = default(T);
-            IList<PropertyInfo> properties = null;
-            using (SqlConnection connection = new SqlConnection(cnStr))
+            var type = new T();
+            IList<PropertyInfo> properties = type.GetType().GetProperties();
+            var tableName = (TableNameAttribute)type.GetType().GetCustomAttribute(typeof(TableNameAttribute));
+            if (tableName == null) { throw new NullReferenceException("Table name attribute of type cannot be empty or null."); }
+            List<string> columnsStrings = GetColumnsStringsOnAttributesProperties(properties);
+
+            using (var connection = new SqlConnection(_cnStr))
             {
-                var query = QueryManager.GenerateSelectAllQuery(type, ref properties, profileId, fieldId);
-                using (SqlCommand command = new SqlCommand(query, connection))
+                var query = QueryManager.GenerateSelectOneFild(tableName.Name, columnsStrings, profileId, fieldId);
+                using (var command = new SqlCommand(query, connection))
                 {
                     connection.Open();
                     SqlDataReader reader = command.ExecuteReader();
@@ -81,24 +97,29 @@ namespace SavingSystem.MsSQLRepository
             return items.First();
         }
 
+
         public bool Add(int profileId, T type)
         {
-            List<ColumnNameAttribute> attributes = null;
-            using (SqlConnection connection = new SqlConnection(cnStr))
-            {
-                var query = QueryManager.GenerateInsertQuery(type, ref attributes);
-                SqlCommand command = new SqlCommand(query, connection);
+            IList<PropertyInfo> properties = type.GetType().GetProperties();
+            var tableName = (TableNameAttribute)type.GetType().GetCustomAttribute(typeof(TableNameAttribute));
+            if (tableName == null) { throw new NullReferenceException("Table name attribute of type cannot be empty or null."); }
+            List<string> columnsStrings = GetColumnsStringsOnAttributesProperties(properties);
 
-                foreach (var attribute in attributes)
+            using (var connection = new SqlConnection(_cnStr))
+            {
+                var query = QueryManager.GenerateInsertQuery(tableName.Name, columnsStrings);
+                var command = new SqlCommand(query, connection);
+
+                foreach (var column in columnsStrings)
                 {
-                    var propValue = type.GetType().GetProperty(attribute.PropertyName).GetValue(type);
+                    var propValue = type.GetType().GetProperty(column).GetValue(type);
                     if (propValue != null)
                     {
-                        command.Parameters.AddWithValue("@" + attribute.ColumnName, propValue);
+                        command.Parameters.AddWithValue("@" + column, propValue);
                     }
                     else
                     {
-                        command.Parameters.AddWithValue("@" + attribute.ColumnName, DBNull.Value);
+                        command.Parameters.AddWithValue("@" + column, DBNull.Value);
                     }
                 }
                 connection.Open();
@@ -111,9 +132,7 @@ namespace SavingSystem.MsSQLRepository
 
         public abstract bool Update(int profileId, int itemId, T item);
         public abstract bool Delete(int profileId, int itemId);
-
-
-
+        
 
         #region Help methods
         protected IList<T> ReadData<T>(SqlDataReader reader, IList<string> columns, IList<PropertyInfo> properties) where T : new()
@@ -159,10 +178,10 @@ namespace SavingSystem.MsSQLRepository
 
         protected void DeleteItemInTableById(string tableName, int id)
         {
-            using (SqlConnection connection = new SqlConnection(cnStr))
+            using (var connection = new SqlConnection(_cnStr))
             {
                 string query = string.Format("Delete from {0} where Id = {1}", tableName, id);
-                SqlCommand command = new SqlCommand(query, connection);
+                var command = new SqlCommand(query, connection);
                 connection.Open();
                 command.ExecuteNonQuery();
                 connection.Close();
@@ -177,7 +196,35 @@ namespace SavingSystem.MsSQLRepository
             connection.Close();
         }
 
-        
+
+        protected List<string> GetColumnsStringsOnAttributesProperties(IEnumerable<PropertyInfo> properties)
+        {
+            var columnsNames = new List<string>();
+            foreach (var property in properties)
+            {
+                Attribute attribute = property.GetCustomAttribute(typeof(ColumnNameAttribute));
+                var columnNameAttribute = (ColumnNameAttribute)attribute;
+                if (columnNameAttribute != null)
+                {
+                    columnsNames.Add(columnNameAttribute.ColumnName);
+                }
+            }
+            return columnsNames;
+        }
+
+
+        protected IList<PropertyInfo> GetPropertiesOnAttributes(IEnumerable<PropertyInfo> properties)
+        {
+            var propertiesOnAttr = new List<PropertyInfo>();
+            foreach (var property in properties)
+            {
+                if (property.GetCustomAttribute(typeof(ColumnNameAttribute)) != null)
+                {
+                    propertiesOnAttr.Add(property);
+                }
+            }
+            return propertiesOnAttr;
+        }
         #endregion
     }
 }
--- SavingSystem.QueryBuilder/QueryManager.cs
+++ SavingSystem.QueryBuilder/QueryManager.cs
@@ -1,68 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Globalization;
-using System.Reflection;
 using System.Text;
-using SavingSystem.Comon.Attributes;
 
 namespace SavingSystem.QueryBuilder
 {
     public class QueryManager
     {
-        public string GenerateSelectAllQuery<T>(T type, ref IList<PropertyInfo> properties)
+        public string GenerateSelectAllQuery(string tableName, IList<string> columnsStrings)
         {
-            var tableName = (TableNameAttribute)type.GetType().GetCustomAttribute(typeof(TableNameAttribute));
-            if (tableName.Name == null)
-            {
-                return null;
-            }
-            properties = type.GetType().GetProperties();
-            var attributes = GetAttributesProperties(properties);
             var sb = new StringBuilder();
 
             sb.Append("SELECT");
             sb.Append(" ");
-            foreach (var attribute in attributes)
+            foreach (var column in columnsStrings)
             {
-                if (attribute != null)
-                {
-                    sb.Append(attribute.ColumnName);
-                    sb.Append(", ");
-                }
+                sb.Append(column);
+                sb.Append(", ");
             }
             sb.Remove(sb.Length - 2, 1);
             sb.Append("FROM");
             sb.Append(" ");
-            sb.Append(tableName.Name);
+            sb.Append(tableName);
 
             return sb.ToString();
         }
 
-        public string GenerateSelectAllQuery<T>(T type, ref IList<PropertyInfo> properties, int profileId)
+
+        public string GenerateSelectAllQuery(string tableName, IList<string> columnsStrings, int profileId)
         {
-            var tableName = (TableNameAttribute)type.GetType().GetCustomAttribute(typeof(TableNameAttribute));
-            if (tableName.Name == null)
-            {
-                return null;
-            }
-            properties = type.GetType().GetProperties();
-            var attributes = GetAttributesProperties(properties);
             var sb = new StringBuilder();
 
             sb.Append("SELECT");
             sb.Append(" ");
-            foreach (var attribute in attributes)
+            foreach (var column in columnsStrings)
             {
-                if (attribute != null)
-                {
-                    sb.Append(attribute.ColumnName);
-                    sb.Append(", ");
-                }
+                sb.Append(column);
+                sb.Append(", ");
             }
             sb.Remove(sb.Length - 2, 1);
             sb.Append("FROM");
             sb.Append(" ");
-            sb.Append(tableName.Name);
+            sb.Append(tableName);
             sb.Append(" ");
             sb.Append("WHERE ProfileId = ");
             sb.Append(profileId.ToString(CultureInfo.InvariantCulture));
@@ -71,32 +49,23 @@ namespace SavingSystem.QueryBuilder
             return sb.ToString();
         }
 
-        public string GenerateSelectAllQuery<T>(T type, ref IList<PropertyInfo> properties, int profileId, int fieldId)
+
+        public string GenerateSelectOneFild(string tableName, IList<string> columnsStrings, int profileId, int fieldId)
         {
-            var tableName = (TableNameAttribute)type.GetType().GetCustomAttribute(typeof(TableNameAttribute));
-            if (tableName.Name == null)
-            {
-                return null;
-            }
-            properties = type.GetType().GetProperties();
-            var attributes = GetAttributesProperties(properties);
             var sb = new StringBuilder();
 
             sb.Append("SELECT");
             sb.Append(" ");
 
-            foreach (var attribute in attributes)
+            foreach (var column in columnsStrings)
             {
-                if (attribute != null)
-                {
-                    sb.Append(attribute.ColumnName);
-                    sb.Append(", ");
-                }
+                sb.Append(column);
+                sb.Append(", ");
             }
             sb.Remove(sb.Length - 2, 1);
             sb.Append("FROM");
             sb.Append(" ");
-            sb.Append(tableName.Name);
+            sb.Append(tableName);
             sb.Append(" ");
             sb.Append("WHERE ProfileId = ");
             sb.Append(profileId.ToString(CultureInfo.InvariantCulture));
@@ -109,29 +78,20 @@ namespace SavingSystem.QueryBuilder
             return sb.ToString();
         }
 
-        public string GenerateInsertQuery<T>(T type, ref List<ColumnNameAttribute> attributes)
+
+        public string GenerateInsertQuery(string tableName, IList<string> columnsStrings)
         {
-            var tableName = (TableNameAttribute)type.GetType().GetCustomAttribute(typeof(TableNameAttribute));
-            if (tableName.Name == null)
-            {
-                return null;
-            }
-            var properties = type.GetType().GetProperties();
-            attributes = GetAttributesProperties(properties);
             var sb = new StringBuilder();
 
             sb.Append("INSERT INTO");
             sb.Append(" ");
-            sb.Append(tableName.Name);
+            sb.Append(tableName);
             sb.Append(" ");
             sb.Append("(");
-            foreach (var attribute in attributes)
+            foreach (var column in columnsStrings)
             {
-                if (attribute != null)
-                {
-                    sb.Append(attribute.ColumnName);
-                    sb.Append(", ");
-                }
+                sb.Append(column);
+                sb.Append(", ");
             }
             sb.Remove(sb.Length - 2, 2);
             sb.Append(")");
@@ -139,14 +99,11 @@ namespace SavingSystem.QueryBuilder
             sb.Append("VALUES");
             sb.Append(" ");
             sb.Append("(");
-            foreach (var attribute in attributes)
+            foreach (var column in columnsStrings)
             {
-                if (attribute != null)
-                {
-                    sb.Append("@");
-                    sb.Append(attribute.ColumnName);
-                    sb.Append(", ");
-                }
+                sb.Append("@");
+                sb.Append(column);
+                sb.Append(", ");
             }
             sb.Remove(sb.Length - 2, 2);
             sb.Append(")");
@@ -155,35 +112,25 @@ namespace SavingSystem.QueryBuilder
         }
 
 
-        public string GenerateUpdateQuery<T>(T type, int fieldId)
+        public string GenerateUpdateQuery(string tableName, IList<string> columnsStrings, int fieldId)
         {
-            var tableName = (TableNameAttribute)type.GetType().GetCustomAttribute(typeof(TableNameAttribute));
-            if (tableName.Name == null)
-            {
-                return null;
-            }
-            var properties = type.GetType().GetProperties();
-            var attributes = GetAttributesProperties(properties);
             var sb = new StringBuilder();
 
             sb.Append("UPDATE");
             sb.Append(" ");
-            sb.Append(tableName.Name);
+            sb.Append(tableName);
             sb.Append(" ");
             sb.Append("SET");
             sb.Append(" ");
-            foreach (var attribute in attributes)
+            foreach (var column in columnsStrings)
             {
-                if (attribute != null)
-                {
-                    sb.Append(attribute.ColumnName);
-                    sb.Append(" ");
-                    sb.Append("=");
-                    sb.Append(" ");
-                    sb.Append("@");
-                    sb.Append(attribute.ColumnName);
-                    sb.Append(", ");
-                }
+                sb.Append(column);
+                sb.Append(" ");
+                sb.Append("=");
+                sb.Append(" ");
+                sb.Append("@");
+                sb.Append(column);
+                sb.Append(", ");
             }
             sb.Remove(sb.Length - 2, 1);
             sb.Append("WHERE Id = ");
@@ -193,18 +140,13 @@ namespace SavingSystem.QueryBuilder
         }
 
 
-        public string GenerateDeleteQuery<T>(T type, int fieldId)
+        public string GenerateDeleteQuery(string tableName, int fieldId)
         {
-            var tableName = (TableNameAttribute)type.GetType().GetCustomAttribute(typeof(TableNameAttribute));
-            if (tableName.Name == null)
-            {
-                return null;
-            }
             var sb = new StringBuilder();
 
             sb.Append("DELETE FROM");
             sb.Append(" ");
-            sb.Append(tableName.Name);
+            sb.Append(tableName);
             sb.Append(" ");
             sb.Append("WHERE");
             sb.Append(" ");
@@ -215,22 +157,6 @@ namespace SavingSystem.QueryBuilder
             sb.Append(fieldId.ToString(CultureInfo.InvariantCulture));
 
             return sb.ToString();
-        }
-
-
-        private List<ColumnNameAttribute> GetAttributesProperties(IList<PropertyInfo> properties)
-        {
-            var attributesProperties = new List<ColumnNameAttribute>();
-            foreach (var property in properties)
-            {
-                Attribute attribute = property.GetCustomAttribute(typeof(ColumnNameAttribute));
-                var columnNameAttribute = (ColumnNameAttribute)attribute;
-                if (columnNameAttribute != null)
-                {
-                    attributesProperties.Add(columnNameAttribute);
-                }
-            }
-            return attributesProperties;
         }
     }
 }
